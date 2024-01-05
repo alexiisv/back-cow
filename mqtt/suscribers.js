800b0cdbc62d773ac0496a83eace637afe1475e6
alexiisv <alexisv@udenar.edu.co>
2024-01-04 23:34:21 -0500

    DB completa

--- mqtt/suscribers.js
+++ mqtt/suscribers.js
@@ -8,17 +8,13 @@ const listenMessage = async (client, io) => {
 
         if(payload?.decoded_payload) {
             console.log('update data');
-<<<<<<< HEAD
-            // console.log(payload);
-=======
             
             const dateUTC = new Date(payload.received_at)
             dateUTC.setHours(dateUTC.getHours() - 5)
             const dateUTC5 = dateUTC.toISOString()
             payload.received_at = dateUTC5
             
-            console.log(payload);
->>>>>>> 7b535e7d6c84c2e3319017f35874832972eac082
+            // console.log(payload);
 
             const dataVacas = JSON.stringify(payload.decoded_payload, null, 2);
             // fs.appendFileSync('measure.txt',  dataVacas + '\n');
@@ -29,12 +25,58 @@ const listenMessage = async (client, io) => {
                 c02_scd: payload.decoded_payload.Co2_scd,
                 temp_scd: payload.decoded_payload.temp_scd,  
                 hum_scd: payload.decoded_payload.hum_scd,  
-                heart_valid: payload.decoded_payload.heart_valid,  
+                
+                heart:{
+                    value: payload.decoded_payload.heart,  
+                    valid: payload.decoded_payload.heart_valid,  
+                },
+                spo2:{
+                    value: payload.decoded_payload.spo2,  
+                    valid: payload.decoded_payload.spo2_valid,  
+                },
+
+                temp_max: payload.decoded_payload.temp_max,  
+                temp_ambi: payload.decoded_payload.temp_ambi,  
+                temp_objet: payload.decoded_payload.temp_objet,  
+                
+                pasos: {
+                    detec: payload.decoded_payload.detec_pas,
+                    cont: payload.decoded_payload.cont_pas,
+                },
+
+                temp_nicla: payload.decoded_payload.temp_nicla,
+                hum_nicla: payload.decoded_payload.temp_nicla,
+                presion: payload.decoded_payload.presion,
+                gasesg: payload.decoded_payload.gasesg,
+        
                 gyro: { 
                     x: payload.decoded_payload.gyroX,
                     y: payload.decoded_payload.gyroY,
                     z: payload.decoded_payload.gyroZ,
-                }
+                },
+                mag: { 
+                    x: payload.decoded_payload.magX,
+                    y: payload.decoded_payload.magY,
+                    z: payload.decoded_payload.magZ,
+                },
+                activ: payload.decoded_payload.activ,
+                iaq_regul: payload.decoded_payload.iaq_regul,
+                iaq_estat: payload.decoded_payload.iaq_estat,
+                
+                compu_org_volat: payload.decoded_payload.compu_org_volat,
+                co2_nicla: payload.decoded_payload.co2_nicla,
+
+                ori: { 
+                    x: payload.decoded_payload.oriX,
+                    y: payload.decoded_payload.oriY,
+                    z: payload.decoded_payload.oriZ,
+                },
+                
+                Cont: payload.decoded_payload.Cont,
+                Batt: payload.decoded_payload.Batt,
+
+                received_at:payload.received_at
+
 
             });
             await record.save();
