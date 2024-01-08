b779e6f86687152cc42857199c47e6dc2496bf5a
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-01-08 17:55:03 -0500

    Instalando standard.js y formateando el proyecto

--- controllers/cows.controllers.js
+++ controllers/cows.controllers.js
@@ -1,12 +1,11 @@
-const record = require("../models/record");
-const Record = require("../models/record");
+const Record = require('../models/record')
 
-const recordsGet = async(req = request, res = response) => {
-    const records = await Record.find( {} );
+const recordsGet = async (req, res) => {
+  const records = await Record.find({})
 
-    res.json( records );
+  res.json(records)
 }
 
 module.exports = {
-    recordsGet
+  recordsGet
 }
