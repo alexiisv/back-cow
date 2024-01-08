14f6eae7ad1d9cac506506e51b7d487d16876150
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-01-08 16:54:34 -0500

    Formatear el suscriber

--- mqtt/suscribers.js
+++ mqtt/suscribers.js
@@ -13,19 +13,12 @@ const listenMessage = async (client, io) => {
             dateUTC.setHours(dateUTC.getHours() - 5)
             const dateUTC5 = dateUTC.toISOString()
             payload.received_at = dateUTC5
-            
-            // console.log(payload);
-
-            const dataVacas = JSON.stringify(payload.decoded_payload, null, 2);
-            // fs.appendFileSync('measure.txt',  dataVacas + '\n');
-            fs.appendFileSync('vaca0712.txt',  dataVacas + ',' + '\n');
 
             const record = new Record({
                 aid_Vaca: payload.decoded_payload.aid_Vaca,
                 c02_scd: payload.decoded_payload.Co2_scd,
                 temp_scd: payload.decoded_payload.temp_scd,  
                 hum_scd: payload.decoded_payload.hum_scd,  
-                
                 heart:{
                     value: payload.decoded_payload.heart,  
                     valid: payload.decoded_payload.heart_valid,  
@@ -34,21 +27,17 @@ const listenMessage = async (client, io) => {
                     value: payload.decoded_payload.spo2,  
                     valid: payload.decoded_payload.spo2_valid,  
                 },
-
                 temp_max: payload.decoded_payload.temp_max,  
                 temp_ambi: payload.decoded_payload.temp_ambi,  
                 temp_objet: payload.decoded_payload.temp_objet,  
-                
                 pasos: {
                     detec: payload.decoded_payload.detec_pas,
                     cont: payload.decoded_payload.cont_pas,
                 },
-
                 temp_nicla: payload.decoded_payload.temp_nicla,
                 hum_nicla: payload.decoded_payload.temp_nicla,
                 presion: payload.decoded_payload.presion,
                 gasesg: payload.decoded_payload.gasesg,
-        
                 gyro: { 
                     x: payload.decoded_payload.gyroX,
                     y: payload.decoded_payload.gyroY,
@@ -62,10 +51,8 @@ const listenMessage = async (client, io) => {
                 activ: payload.decoded_payload.activ,
                 iaq_regul: payload.decoded_payload.iaq_regul,
                 iaq_estat: payload.decoded_payload.iaq_estat,
-                
                 compu_org_volat: payload.decoded_payload.compu_org_volat,
                 co2_nicla: payload.decoded_payload.co2_nicla,
-
                 ori: { 
                     x: payload.decoded_payload.oriX,
                     y: payload.decoded_payload.oriY,
@@ -74,14 +61,11 @@ const listenMessage = async (client, io) => {
                 
                 Cont: payload.decoded_payload.Cont,
                 Batt: payload.decoded_payload.Batt,
-
                 received_at:payload.received_at
-
-
             });
             await record.save();
 
-            io.emit('dataVacas', payload.decoded_payload);
+            io.emit('dataVacas', record);
         }
     });
 }
