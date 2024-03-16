461d131c1a6d846bd031e181adc05d6b586beaed
Yorth Ortegon <yorth21@gmail.com>
2024-03-15 21:49:22 -0500

    Add jwtMiddleware to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import { collaresRoutes } from './collares.routes.js'
 import { usersRoutes } from './users.routes.js'
+import { jwtMiddleware } from '../middlewares/jwt.middleware.js'
 
 export const Routes = Router()
+
+Routes.use(jwtMiddleware)
 
 Routes
   .use('/collares', collaresRoutes)
